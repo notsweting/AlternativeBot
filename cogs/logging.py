1bcdc2c720b2841276f29a096e8d036c31e319dc
ThisIsanAlt <50222844+ThisIsanAlt@users.noreply.github.com>
2020-12-02 22:11:52 -0500

    on message delete is working!

--- cogs/logging.py
+++ cogs/logging.py
@@ -4,6 +4,7 @@ from discord.ext import commands, tasks, menus
 from discord.ext.commands.cooldowns import BucketType
 import sqlite3
 from discord.ext import menus
+import time
 
 '''
 Table definitions:
@@ -18,28 +19,147 @@ TABLE LOGGING, columns (ServerID, LoggingToggle, LoggingChannelID, \
 '''
 
 class MyMenu(menus.Menu):
+    async def return_values(self):
+        connection = sqlite3.connect('AltBotDataBase.db')
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM LOGGING WHERE ServerID = ?', (self.guild_id,))
+        info = cursor.fetchone()
+        connection.close()
+        listtoreturn = []
+        if info[1] == True:
+            listtoreturn.append(':white_check_mark: Main logging toggle on. Disable with the :stop_button: reaction.')
+        else:
+            listtoreturn.append(':x: Main logging toggle off. Enable with the :play_pause: reaction.')
+        if info[2] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :one: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :one: reaction.')
+        if info[3] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :two: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :two: reaction.')
+        if info[4] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :three: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :three: reaction.')
+        if info[5] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :three: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :three: reaction.')
+        if info[6] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :one: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :one: reaction.')
+        if info[7] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :two: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :two: reaction.')
+        if info[8] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :three: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :three: reaction.')
+        if info[9] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :three: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :three: reaction.')
+        if info[10] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :one: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :one: reaction.')
+        if info[11] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :two: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :two: reaction.')
+        if info[12] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :three: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :three: reaction.')
+        if info[13] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :three: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :three: reaction.')
+        if info[14] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :one: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :one: reaction.')
+        if info[15] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :two: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :two: reaction.')
+        if info[16] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :three: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :three: reaction.')
+        if info[17] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :three: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :three: reaction.')
+        if info[18] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :one: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :one: reaction.')
+        if info[19] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :two: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :two: reaction.')
+        if info[20] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :three: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :three: reaction.')
+        if info[21] == True:
+            listtoreturn.append(':white_check_mark: Enabled. Toggle with the :three: reaction.')
+        else:
+            listtoreturn.append(':x: Disabled. Toggle with the :three: reaction.')
+        return tuple(listtoreturn)
 
     async def send_initial_message(self, ctx, channel):
-        self.connection = sqlite3.connect('AltBotDataBase.db')
-        self.cursor = self.connection.cursor()
         self.menupage = 1
-        embed = discord.Embed (title=f'Logging Toggles for {ctx.guild.name}', description='Use the reactions to navigate through the available options!')
+        self.guild_name = ctx.guild.name
+        self.guild_id = ctx.guild.id
+        self.embed = discord.Embed (title=f'Logging Toggles for {ctx.guild.name}', description='Use the reactions to navigate through the available options!')
+        info = self.return_values()
+        self.embed.add_field(name='On message delete toggle:', value = info[1])
+        self.embed.add_field(name='On bulk message delete toggle:', value = info[2])
+        return await channel.send(embed=self.embed)
 
-        embed.add_field(name='On message delete toggle:', value='Enabled. Disable with the :one: reaction.')
-        return await channel.send(embed = embed)
-
-    @menus.button('\N{keycap ten}')
-    async def on_ten(self, payload):
+    @menus.button('\U00000031\U0000fe0f\U000020e3')
+    async def on_one(self, payload):
+        connection = sqlite3.connect('AltBotDataBase.db')
+        cursor = connection.cursor()
         if self.menupage == 1:
-            self.cursor.execute('SELECT OnMsgDeleteToggle FROM LOGGING WHERE ServerID = ?', (payload.guild.id,))
-            info = self.cursor.fetchone()
+            cursor.execute('SELECT OnBulkMsgDeleteToggle FROM LOGGING WHERE ServerID = ?', (payload.guild_id,))
+            info = cursor.fetchone()
             info = info[0]
             if info == None or info == False: 
                 info = True
             else:
                 info = False
-            self.cursor.execute('UPDATE LOGGING SET OnMsgDeleteToggle = ? WHERE ServerID = ?', (info, payload.guild.id))
-            await self.message.edit(content='hi')
+            cursor.execute('UPDATE LOGGING SET OnMsgDeleteToggle = ? WHERE ServerID = ?', (info, payload.guild_id))
+            info = self.return_values()
+            self.embed.insert_field_at(0, name='On message delete toggle:', value=info[1])
+            self.embed.remove_field(1)
+            cursor.execute('UPDATE LOGGING SET OnMsgDeleteToggle = ? WHERE ServerID = ?', (info, payload.guild_id))
+            await self.message.edit(embed=self.embed)
+            connection.commit()
+
+    @menus.button('\U00000032\U0000fe0f\U000020e3')
+    async def on_two(self, payload):
+        connection = sqlite3.connect('AltBotDataBase.db')
+        cursor = connection.cursor()
+        if self.menupage == 1:
+            cursor.execute('SELECT OnBulkMsgDeleteToggle FROM LOGGING WHERE ServerID = ?', (payload.guild_id,))
+            info = cursor.fetchone()
+            info = info[0]
+            if info == None or info == False: 
+                info = True
+            else:
+                info = False
+            info = self.return_values()
+            self.embed.insert_field_at(1, name='On bulk message delete toggle:', value=info[2])
+            self.embed.remove_field(2)
+            cursor.execute('UPDATE LOGGING SET OnMsgDeleteToggle = ? WHERE ServerID = ?', (info, payload.guild_id))
+            await self.message.edit(embed=self.embed)
+            connection.commit()
 
 class Logging(commands.Cog):
     def __init__(self, bot):
@@ -73,7 +193,7 @@ class Logging(commands.Cog):
             cursor.execute(f'INSERT INTO LOGGING(ServerID, LoggingChannelID) VALUES (?, ?)', (ctx.guild.id, channel.id))
             await ctx.send(f':thumbsup: Bound {channel.mention} to {ctx.guild.name} as logging channel.')
         elif discord.utils.get(ctx.guild.channels, id=int(channel.id)) != None:
-            await ctx.send('A logginc channel has already been bound to this server! Are you sure you want to continue? `yes/no`')
+            await ctx.send('A logging channel has already been bound to this server! Are you sure you want to continue? `yes/no`')
             def check(message : discord.Message) -> bool:
                 return message.author == ctx.author and message.channel == ctx.channel
             
@@ -98,22 +218,43 @@ class Logging(commands.Cog):
     async def on_message_delete(self, message):
         connection = sqlite3.connect('AltBotDataBase.db')
         cursor = connection.cursor()
-        cursor.execute('SELECT OnMsgDeleteToggle FROM LOGGING WHERE ServerID = ?', (message.guild.id,))
+        cursor.execute('SELECT * FROM LOGGING WHERE ServerID = ?', (message.guild.id,))
         info = cursor.fetchone()
+        connection.close()
         if info == None:
             pass
         else:
-            if info[0] == None or info[0] == False:
+            if info[3] == None or info[3] == False:
                 pass
             else:
                 channel = discord.utils.get(message.guild.channels, id=info[2])
                 try:
-                    embed = discord.Embed(title = 'Message deleted in {message.channel}', description = message.content, timestamp = message.message.created_at)
-                    embed.set_footer(text=f'{message.author} | Support: https://discord.gg/33utPs9', icon_url=message.author.avatar_url)
+                    embed = discord.Embed(title = f'Message deleted in #{message.channel}', description = message.content, timestamp = message.created_at, colour = 0xFF5353)
+                    embed.set_footer(text=f'Author: {message.author} | Support: https://discord.gg/33utPs9', icon_url=message.author.avatar_url)
                     await channel.send(embed=embed)
                 except:
                     pass
+    
+    @commands.Cog.listener()
+    async def on_raw_bulk_message_delete(self, payload):
+        connection = sqlite3.connect('AltBotDataBase.db')
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM LOGGING WHERE ServerID = ?', (payload.guild_id,))
+        info = cursor.fetchone()
         connection.close()
+        if info == None:
+            pass
+        else:
+            if info[4] == None or info[4] == False:
+                pass
+            else:
+                channel = self.bot.get_channel(payload.channel_id)
+                try:
+                    embed = discord.Embed(title = f'Bulk message delete in #{channel.name}', description = 'A bulk message delete was run!', timestamp = time.time(), colour = 0xFF5353)
+                    embed.set_footer(text=f'Support: https://discord.gg/33utPs9', icon_url=self.bot.avatar_url)
+                    await channel.send(embed=embed)
+                except:
+                    pass
 
 def setup(bot):
     bot.add_cog(Logging(bot))
